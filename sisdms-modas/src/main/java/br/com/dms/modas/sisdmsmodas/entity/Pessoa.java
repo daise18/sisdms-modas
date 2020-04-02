056e00f09c54ee43ca7db462f0a12cf386d06eea
daise18 <daisemarianosilva@gmail.com>
2020-04-01 22:39:37 -0300

    Adicionando banco de dados embarcado H2 com Spring Boot e JPA

---
+++ sisdms-modas/src/main/java/br/com/dms/modas/sisdmsmodas/entity/Pessoa.java
@@ -0,0 +1,112 @@
+package br.com.dms.modas.sisdmsmodas.entity;
+
+import javax.persistence.*;
+
+@Entity(name = "pessoa")
+public class Pessoa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_pessoa")
+    private Long id;
+    private String nome;
+
+    @Column(name = "data_nascimento")
+    private String dataNascimento;
+    private String endereco;
+    private String bairro;
+    private String cep;
+    private String estado;
+    private String pais;
+    private String rg;
+    private String cpf;
+    private String cnh;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getCnh() {
+        return cnh;
+    }
+
+    public void setCnh(String cnh) {
+        this.cnh = cnh;
+    }
+}
