7fd74b249d81699019cc8a53308e556838f38ffc
daise18 <daisemarianosilva@gmail.com>
2020-04-01 21:49:29 -0300

    versão inicial

---
+++ sisdms-modas/src/main/java/br/com/dms/modas/sisdmsmodas/SisdmsModasApplication.java
@@ -0,0 +1,13 @@
+package br.com.dms.modas.sisdmsmodas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisdmsModasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisdmsModasApplication.class, args);
+	}
+
+}
---
+++ sisdms-modas/src/test/java/br/com/dms/modas/sisdmsmodas/SisdmsModasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dms.modas.sisdmsmodas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisdmsModasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
